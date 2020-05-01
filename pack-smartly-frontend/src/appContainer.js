0704ac9af1c60f99e5b40250bd34c980909ab270
github username <you@example.com>
2020-04-30 22:42:04 -0400

    extend eventlistener,populate items properties with the returned data

--- pack-smartly-frontend/src/appContainer.js
+++ pack-smartly-frontend/src/appContainer.js
@@ -1,22 +1,33 @@
 class AppContainer {
-    items = []
+    static items = []
     trips = []
     url = "http://localhost:3000"
     packingList= {}
 
     bindEventListeners() {
         const btn = document.getElementById('createPackingList')
-        debugger
+        btn.addEventListener('click', this.getCustomizedItems.bind(this))
     }
+
+    getCustomizedItems(){ 
+        const chosenTrip= document.getElementById('chosenTrip')
+        let customizedItems=AppContainer.items.filter(item => item.trip === chosenTrip)
+        return customizedItems 
+    }
+
 
     getItems(){
         // make a fetch request to /weathers
         console.log('something')
         fetch(this.url + '/items')
         .then(resp => resp.json())
-        .then(data => console.log(data))
-        // populate the weathers and tripStyles properties with the returned data
-        // call renderWeathers
+        // populate the items properties with the returned data
+        .then(data => {
+            data.forEach(item => {
+                new Item(item.name, item.trip_id)
+            })
+        })
+        // call renderItems
         .catch(err => alert(err))
     }
 
--- pack-smartly-frontend/src/item.js
+++ pack-smartly-frontend/src/item.js
@@ -2,5 +2,6 @@ class Item {
     constructor(name, trip_id){
         this.name = name;
         this.trip_id = trip_id;
+        AppContainer.items.push(this)
     }
 }
